2bf04708086abab45b138141078d4062795d77ab
Adam Selker <adamselker@gmail.com>
2017-11-29 20:56:11 -0500

    Cleaning the cleaner.

--- clean_file.py
+++ clean_file.py
@@ -1,12 +1,14 @@
-from neurons import letters
+from neuronBlock import letters
 
 fIn = open("moby_dick.txt", "r")
 fOut = open("moby_dick_cleaned.txt", "w")
 
 orig = fIn.read()
 
+# Replace newlines with spaces, and convert fancy quotes and dashes to normal ones
 oneline = orig.replace("\r","").replace("\n"," ").replace("“","\"").replace("”","\"").replace("’","'").replace("-","-").lower()
 
+# Replace all other unknown chars with underscores
 cleaned = "".join([(x if x in letters else "_") for x in oneline])
 
 while cleaned.find("  ") != -1:
